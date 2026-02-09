68f18e99b20fdd8e1b5067ebd408a47aaa3660d9
Sofiia-Khrystyna Borysiuk <sofiia@fluxguide.com>
2026-02-10 02:42:26 +0800

    simple file uploading and retrieval + related documentation

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# modas
+# MoDaS
+
+For setup: https://docs.streamlit.io/get-started/installation/command-line
+
+For uploading files: https://docs.streamlit.io/develop/api-reference/widgets/st.file_uploader
+
--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Hello world")
+uploaded_files = st.file_uploader("Upload data", accept_multiple_files=True, type="csv")
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
