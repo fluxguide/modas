0d9a0df784d08409ab694e4b8180b12b1cf4aa09
Sofiia-Khrystyna Borysiuk <sofiia@fluxguide.com>
2026-02-10 15:55:14 +0800

    table editing added

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import pandas as pd
 uploaded_files = st.file_uploader("Upload data", accept_multiple_files=True, type="csv")
 for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
-    st.write(df)
+    edited_df = st.data_editor(df)
