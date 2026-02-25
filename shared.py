8f6ab602724f2eed864a2326c9383bc563283b69
Sofiia-Khrystyna Borysiuk <sofiia@fluxguide.com>
2026-02-25 10:29:29 +0800

    hide header anchor link icons

--- shared.py
+++ shared.py
@@ -33,6 +33,19 @@ def setup_page(
     show_top_bar=True, top_bar_links=None, active_page=None, top_bar_right_button=None
 ):
     st.set_page_config(layout="wide")
+
+    st.markdown(
+        """
+        <style>
+        /* Hides the link icon that appears on hover for headers */
+        [data-testid="stHeaderActionElements"] {
+            display: none;
+        }
+        </style>
+    """,
+        unsafe_allow_html=True,
+    )
+
     with open("style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     if show_top_bar:
